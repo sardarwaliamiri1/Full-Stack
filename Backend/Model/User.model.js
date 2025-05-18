72e6eb232b2d578b9d43f5f1fb5c9ceaf3ab983b
sardarwaliamiri1 <sardarwaliamiry@gmail.com>
2025-05-18 10:27:05 +0430

    User schema

---
+++ Backend/Model/User.model.js
@@ -0,0 +1,23 @@
+import { Schema ,model} from "mongoose";
+import { type } from "os";
+const userSchema = new Schema({
+      name:{
+            type: String,
+            required: true,
+            trim: true      
+      },
+      email:{
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+      },
+      password:{
+            type: String,
+            required: true,
+            trim: true
+      },
+
+})
+
+const User = model('User', userSchema);
--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,12 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.MONGO_URI).then(() => {
+      console.log('MongoDB connected');
+}).catch((err) => {
+      console.log('MongoDB connection error:', err);
+});
 const app= express();
 
 app.listen(3000, ()=> {
